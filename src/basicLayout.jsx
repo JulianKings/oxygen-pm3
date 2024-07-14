68ae371aa6944c4dd00877266e77db980aa8e73d
JulianKings <julli123@hotmail.es>
2024-07-14 12:32:01 +0200

    feat: release basic layout

--- src/basicLayout.jsx
+++ src/basicLayout.jsx
@@ -1,8 +1,52 @@
 import './style/content.css';
+import searchSettingsImage from './assets/search-settings.png';
+import homeImage from './assets/home.png';
+import myPhotosImage from './assets/photos.png';
+import cameraImage from './assets/camera-icon.png';
+import linkedInImage from './assets/linkedin.png';
+import { getNextQuote } from './util/quoteManager';
+import { Outlet } from 'react-router-dom';
 
 function BasicLayout()
 {
-    return <></>;
+    const quote = getNextQuote();
+
+    return <>
+        <header className='header'>
+            <nav className='header__menu'>
+                <div className='header__menu__item header__menu__item--title'><img src={cameraImage} alt='Camera Icon' /><span>Oxygen</span>pics</div>
+                <nav className='header__menu__item'><img src={homeImage} alt='Home' /> Home</nav>
+                <nav className='header__menu__item'><img src={searchSettingsImage} alt='Search settings' /> Search Settings</nav>
+                <nav className='header__menu__item'><img src={myPhotosImage} alt='My photos' />My Photos</nav>
+            </nav>
+
+            <div className='header__quote'>
+                {quote}
+            </div>
+
+            <div className='header__search_bar'>
+                <input type='text' id='search-bar' className='header__search_bar__input' placeholder='Search images...' />
+                <span></span>
+            </div>
+        </header>
+
+        <main className='content'>
+            <Outlet />
+        </main>
+
+        <footer className='footer'>
+            <div className='footer__container'>
+                <div className='footer__text'>
+                    <div className='footer__text__title'><span>Oxygen</span> Pics</div>
+                    <div className='footer__text__subtitle'>© 2024 OXYGEN, All Rights Reserved</div>
+                </div>
+
+                <div className='footer__linkedin'>
+                    <img src={linkedInImage} alt='LinkedIn' />
+                </div>
+            </div>
+        </footer>
+    </>;
 }
 
 export default BasicLayout;
